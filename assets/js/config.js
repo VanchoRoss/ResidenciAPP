8a9e60729473a449a07722bbd2baa7fe60a1c953
VanchoRoss <ivan.rossetti.j@gmail.com>
2026-04-29 00:23:51 -0300

    config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -6,7 +6,7 @@ window.RESIDENCIAPP_CONFIG = {
     mode: 'google-sheet',
     enabled: true,
     // Endpoint /exec de Google Apps Script para recibir aportes.
-    endpoint: 'https://script.google.com/macros/s/AKfycbxaNwbYDNjee8gfrO2GBXwIkW8ppBekaUqv_Bh-XSUvZ2D2bNN261m0HWDWQ4OjGBj9xA/exec',
+    endpoint: 'https://script.google.com/macros/s/AKfycbygt2OyUmyVEjgeIYvEYGEJNAM_QMsRM3NPMz7WOfZ_tP79OAGe3Cssl52fqv8rynpvUA/exec',
     requireEndpoint: false,
     allowImages: true,
     maxImageSizeMB: 3
